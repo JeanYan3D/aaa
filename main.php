f340e62ba4538ecd1ff86041918100e607be3879
Jeanyan <jybabois@gmail.com>
2024-08-19 13:06:14 +1100

    v2

--- main.php
+++ main.php
@@ -13,7 +13,7 @@
   <!-- Bootstrap CSS -->
   <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
   <link href="assets/css/bootstrap-extended.css" rel="stylesheet" />
-  <link href="assets/css/style.css" rel="stylesheet" />
+  
   <link href="assets/css/icons.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
@@ -27,7 +27,7 @@
   <link href="assets/css/light-theme.css" rel="stylesheet" />
   <link href="assets/css/semi-dark.css" rel="stylesheet" />
   <link href="assets/css/header-colors.css" rel="stylesheet" />
-
+  <link href="assets/css/style.css" rel="stylesheet" />
   <title>AAA Runner</title>
 </head>
 
