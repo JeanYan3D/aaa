0fd090afcf1b972ba2a4517b852be6b1827cd08f
Jeanyan <jybabois@gmail.com>
2024-08-24 10:32:33 +1100

    281032

--- usersList.php
+++ usersList.php
@@ -13,7 +13,7 @@ $total_row = mysqli_fetch_assoc($total_result);
 $total_records = $total_row['total'];
 
 // Fetch records with limit and offset
-$sql = "SELECT utilisateur_nom, utilisateur_prenom, utilisateur_email, groupe_id, utilisateur_encopie, utilisateur_actif 
+$sql = "SELECT utilisateur_id, utilisateur_nom, utilisateur_prenom, utilisateur_email, groupe_id, utilisateur_encopie, utilisateur_actif 
         FROM utilisateur LIMIT $limit OFFSET $offset";
 $result = mysqli_query($conn, $sql);
 ?>
@@ -29,28 +29,42 @@ $result = mysqli_query($conn, $sql);
 
             <div class="table-responsive mt-4">
                 <table class="table align-middle mb-0 table-hover table-striped">
-                    <thead class="table-light">
-                        <tr>
-                            <th>Nom</th>
-                            <th>Prénom</th>
-                            <th>Email</th>
-                            <th>Groupe</th>
-                            <th>En copie du programme</th>
-                            <th>Actif</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
-                        <tr>
-                            <td><?php echo $row['utilisateur_nom']; ?></td>
-                            <td><?php echo $row['utilisateur_prenom']; ?></td>
-                            <td><?php echo $row['utilisateur_email']; ?></td>
-                            <td><?php echo getGroupeName($row['groupe_id']); ?></td>
-                            <td><?php echo $row['utilisateur_encopie'] ? '✔' : ''; ?></td>
-                            <td><?php echo $row['utilisateur_actif'] ? '✔' : ''; ?></td>
-                        </tr>
-                        <?php endwhile; ?>
-                    </tbody>
+                <thead class="table-light">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Email</th>
+                        <th>Groupe</th>
+                        <th>En copie du programme</th>
+                        <th>Actif</th>
+                        <th>Actions</th> <!-- New Actions Column -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                    <tr>
+                        <td><?php echo $row['utilisateur_nom']; ?></td>
+                        <td><?php echo $row['utilisateur_prenom']; ?></td>
+                        <td><?php echo $row['utilisateur_email']; ?></td>
+                        <td><?php echo getGroupeName($row['groupe_id']); ?></td>
+                        <td><?php echo $row['utilisateur_encopie'] ? '✔' : ''; ?></td>
+                        <td><?php echo $row['utilisateur_actif'] ? '✔' : ''; ?></td>
+                        <<td>
+                            <!-- Icons with labels for actions -->
+                            <a href="voir.php?id=<?php echo $row['utilisateur_id']; ?>" title="Voir" style="margin-right: 10px;">
+                                <i class="bi bi-eye"></i>
+                            </a>
+                            <a href="modifier.php?id=<?php echo $row['utilisateur_id']; ?>" title="Modifier" style="margin-right: 10px;">
+                                <i class="bi bi-pencil"></i>
+                            </a>
+                            <a href="supprimer.php?id=<?php echo $row['utilisateur_id']; ?>" title="Supprimer" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?');">
+                                <i class="bi bi-trash"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+
                 </table>
             </div>
 
